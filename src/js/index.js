5afd667639d8aad5543191e041e4bf195cd3635e
frostyx-vk <vk.xam@yandex.ru>
2024-04-20 12:47:24 +0700

    change frontend

--- src/js/index.js
+++ src/js/index.js
@@ -22,4 +22,44 @@ document.addEventListener('DOMContentLoaded', () => {
     btnsAdaptForModal.forEach(btn => {
         openModal(btn);
     });
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    const prevBtn = document.querySelector('.prev');
+    prevBtn.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    
+    const nextBtn = document.querySelector('.next');
+    nextBtn.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    };
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    };
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("demo");
+      let captionText = document.getElementById("caption");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace("active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      slides[slideIndex-1].style.textAlign = "center";
+      dots[slideIndex-1].className += "active";
+      captionText.innerHTML = dots[slideIndex-1].alt;
+    };
 });
